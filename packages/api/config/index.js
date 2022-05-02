cd7c6cea65b75ff327e37426309cb520612385dd
Ks Krimi <ny.kalash@gmail.com>
2022-05-02 10:45:59 +0200

    chore(api): remove unused packages

--- packages/api/config/index.js
+++ packages/api/config/index.js
@@ -1,7 +1,6 @@
 export const {
   HTTP_PORT = 8000,
   NODE_ENV = 'developement',
-  ORIGIN_HOST = 'localhost',
   DEV_ORIGIN = 'http://localhost:3000',
   TOKEN_SECRET = '990bf68e6adf1be5f1671bba3bec692056922454',
 
@@ -9,12 +8,14 @@ export const {
   DB_PASSWORD = 'root',
   DB_HOST = 'localhost',
   DB_PORT = 27017,
-  DB_NAME = 'materiels'
+  DB_NAME = 'materiels',
+
+  PROD_ORIGIN
 } = process.env
 
 export const IS_PROD = NODE_ENV === 'production'
 
-export const ORIGIN = IS_PROD ? `http://${ORIGIN_HOST}:80` : DEV_ORIGIN
+export const ORIGIN = IS_PROD ? PROD_ORIGIN : DEV_ORIGIN
 
 // Password URL encoded to escape special characters
 export const MONGO_URI = `mongodb://${DB_USERNAME}:${encodeURIComponent(
